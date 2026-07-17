5b84c36d7c7d66f8d840ab0cce04dcf0a00b35d4
Sumanpreet-Kaur960 <baljindersi2695@gmail.com>
2026-07-17 16:08:42 +0530

    commit7

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import { Routes, Route } from "react-router-dom";
-
-import Dashboard from "./assets/pages/Dashboard";
-import Assessment from "./assets/pages/Assessment";
-import Analytics from "./assets/pages/Analytics";
-import RecoveryPlan from "./assets/pages/RecoveryPlan";
-import Reminders from "./assets/pages/Reminders";
-import Profile from "./assets/pages/Profile";
-import Settings from "./assets/pages/Settings";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/assessment" element={<Assessment />} />
-      <Route path="/analytics" element={<Analytics />} />
-      <Route path="/recovery-plan" element={<RecoveryPlan />} />
-      <Route path="/reminders" element={<Reminders />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/settings" element={<Settings />} />
-    </Routes>
+    <div>
+      <Sidebar />
+    </div>
   );
 }
 
